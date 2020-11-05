78c51990622c91ee9124e19f2333d9490a3181ee
Estrella <Estrella@192.168.0.13>
2020-11-04 21:25:03 -0600

    Numeros pares

---
+++ prueba/src/prueba/Prueba.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author Estrella
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          int numero;
+        for(numero=2;numero<=100; numero++){
+            if(numero%2 ==0)
+                
+                System.out.println(+numero);
+            
+        }
+    }
+    
+}
