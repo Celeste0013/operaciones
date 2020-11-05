b236f1114fb36f152b2ab98c3be1158a15d003bc
Estrella <Estrella@192.168.0.13>
2020-11-04 22:02:40 -0600

    TAREA1.1

---
+++ Proyecto3KMm/src/proyecto3kmm/Proyecto3KMm.java
@@ -0,0 +1,30 @@
+
+package proyecto3kmm;
+
+/**
+ *
+ * @author Estrella
+ */
+public class Proyecto3KMm {
+
+    
+    public static void main(String[] args) {
+      int M=6, T=1, k=-10;
+         if(M>T){
+             System.out.println("Verdadero");
+         } else { 
+             System.out.println("Falso");
+         }
+         if(T/k==-5){
+             System.out.println("Verdadero");
+         }else{
+             System.out.println("Falso");
+         }
+         if (M+T==7 ||M-T==5){
+             System.out.println("Verdadero");
+         }else{
+             System.out.println("Falso");
+         }
+    }
+    
+}
