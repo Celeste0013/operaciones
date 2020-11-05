ca97096185f061ee00018dd5679a5e1bd60737fa
Estrella <Estrella@192.168.0.13>
2020-11-04 22:51:25 -0600

    TAREA1.3

---
+++ operacionesaritmeticas1/src/operacionesaritmeticas1/Operacionesaritmeticas1.java
@@ -0,0 +1,23 @@
+
+package operacionesaritmeticas1;
+
+/**
+ *
+ * @author Estrella
+ */
+public class Operacionesaritmeticas1 {
+
+    
+    public static void main(String[] args) {
+        operaciones ope=new operaciones();
+        ope.setSuma(30, 15, 5, 10);
+        ope.suma();
+        ope.setResta(100, 15, 5, 20);
+        ope.resta();
+        ope.setMulti(2, 2, 5, 3);
+        ope.multi();
+        ope.setDivision(1000, 2, 2, 5);
+        ope.division();
+    }
+    
+}
---
+++ operacionesaritmeticas1/src/operacionesaritmeticas1/operaciones.java
@@ -0,0 +1,54 @@
+
+package operacionesaritmeticas1;
+
+/**
+ *
+ * @author Estrella
+ */
+public class operaciones {
+     int numero1=0, numero2=0, numero3=0, numero4=0, suma=0, resta=0, multi=0, division=0;
+            public operaciones (){
+                
+            }
+            public void setSuma(int a, int b, int c, int d){
+                numero1=a;
+                numero2=b;
+                numero3=c;
+                numero4=d;
+                this.suma=numero1+numero2+numero3+numero4;
+            }
+            public void setResta(int a, int b, int c, int d){
+                numero1=a;
+                numero2=b;
+                numero3=c;
+                numero4=d;
+                this.resta=numero1-numero2-numero3-numero4;
+            }
+            public void setMulti(int a, int b, int c, int d){
+                numero1=a;
+                numero2=b;
+                numero3=c;
+                numero4=d;
+                 this.multi=numero1*numero2*numero3*numero4;
+            }
+            public void setDivision(int a, int b, int c, int d){
+                numero1=a;
+                numero2=b;
+                numero3=c;
+                numero4=d;
+                this.division=numero1/numero2/numero3/numero4;
+            }
+                    public void suma (){
+                    System.out.println("La suma es="+suma);
+                
+            }
+                    public void resta (){
+                    System.out.println("La resta es="+resta);
+            }
+                   public void multi (){
+                    System.out.println("La multiplicacion es="+multi);
+            }
+                   public void division(){
+                    System.out.println("La division es="+division);
+            }
+}
