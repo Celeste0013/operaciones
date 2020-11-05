52d5f77003ff8cca2c171a53c9992bab8fdf84b1
Estrella <Estrella@192.168.0.13>
2020-11-04 23:00:13 -0600

    TAREA1.5

---
+++ City/src/city/City.java
@@ -0,0 +1,21 @@
+
+package city;
+
+/**
+ *
+ * @author Estrella
+ */
+public class City {
+
+    
+    public static void main(String[] args) {
+             String[]ciudad={"Manila","Cartagena","El Cairo","Nairobi","Tokio","Berlin","Palermo","Marsella", "Oslo","Helsinki","Lisboa","Estocolmo","Bogota","Sofia","Denver","Moscu","Londres","Miami","Rio de Janeiro","Paris",};     
+     
+     for(int i=0;i<20;i++){
+        
+        
+     System.out.println(ciudad[i]);
+    }
+    }
+    
+}
