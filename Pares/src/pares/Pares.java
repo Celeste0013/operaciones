3b924de1d05bcfeacf1c1f99fb61d8cca069bb03
Estrella <Estrella@192.168.0.13>
2020-11-04 22:33:46 -0600

    TAREA1.2

--- Pares/src/pares/Pares.java
+++ Pares/src/pares/Pares.java
@@ -9,13 +9,11 @@ public class Pares {
 
     
     public static void main(String[] args) {
-          int numero;
-        for(numero=2;numero<=100; numero++){
-            if(numero%2 ==0)
-                
-                System.out.println(+numero);
-            
-        }
+       int numero;
+       for(numero=2;numero<=100;numero++){
+           if(numero%2==0)
+               System.out.println(+numero);
+       }
     }
     
 }
