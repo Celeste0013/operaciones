a1a585a7b3b61d46fa037e2733e152e9d2a0e616
Estrella <Estrella@192.168.0.13>
2020-11-04 21:20:46 -0600

    Numeros pares

---
+++ Pares/src/pares/Pares.java
@@ -0,0 +1,21 @@
+
+package pares;
+
+/**
+ *
+ * @author Estrella
+ */
+public class Pares {
+
+    
+    public static void main(String[] args) {
+          int numero;
+        for(numero=2;numero<=100; numero++){
+            if(numero%2 ==0)
+                
+                System.out.println(+numero);
+            
+        }
+    }
+    
+}
