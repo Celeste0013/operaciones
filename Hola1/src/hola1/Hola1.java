259d1cbd155af2cc9a7aa8dcbd98e85688cd1b60
Estrella <Estrella@192.168.0.13>
2020-11-04 22:58:16 -0600

    TAREA1.4

---
+++ Hola1/src/hola1/Hola1.java
@@ -0,0 +1,15 @@
+
+package hola1;
+
+/**
+ *
+ * @author Estrella
+ */
+public class Hola1 {
+
+    
+    public static void main(String[] args) {
+     System.out.println("Hola, soy de la clase de programacion orientada a objetos");
+    }
+    
+}
